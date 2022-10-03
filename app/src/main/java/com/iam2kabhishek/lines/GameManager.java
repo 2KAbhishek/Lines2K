0797bec8c196926b8fb44e173b0beaccf35b5d7e
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-03 09:26:31 +0530

    Add game manager

---
+++ app/src/main/java/com/iam2kabhishek/lines/GameManager.java
@@ -0,0 +1,9 @@
+package com.iam2kabhishek.lines;
+
+import android.widget.Button;
+
+import java.util.HashMap;
+
+public class GameManager {
+    public static HashMap<Button, Integer> idAllCells = new HashMap<>();
+}
